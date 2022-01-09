96dacbbdad209a0d2bbae0e598fe0eddf22ff178
akang-pc <akang-pc@AKANG>
2022-01-08 20:36:14 -0800

    add

--- Account/mailFormat.aspx.cs
+++ Account/mailFormat.aspx.cs
@@ -18,21 +18,30 @@ namespace WebApplication1.Account
         {
             //Send Email
             MailMessage Msg = new MailMessage();
-            Msg.From = new MailAddress("Akangtoshi@gmail.com", "SAkang");// Sender details here, replace with valid value
-            Msg.Subject = Subject.Text; // subject of email
-            Msg.To.Add(To.Text); //Add Email id, to which we will send email
-            Msg.Body = EmailMessage.Text;
-            Msg.IsBodyHtml = true;
-            Msg.Priority = MailPriority.High;
-            SmtpClient smtp = new SmtpClient();
-            smtp.UseDefaultCredentials = false; // to get rid of error "SMTP server requires a secure connection"
-            smtp.Host = "smtp.gmail.com";
-            smtp.Port = 587;
-            smtp.Credentials = new System.Net.NetworkCredential("Akangtoshi@gmail.com", "A");// replace with valid value
-            smtp.EnableSsl = true;
-            smtp.Timeout = 20000;
+            try
+            {
 
-            smtp.Send(Msg);
+
+                Msg.From = new MailAddress("Akangtoshi@gmail.com", "CabBoking");// Sender details here, replace with valid value
+                Msg.Subject = Subject.Text; // subject of email
+                Msg.To.Add(To.Text); //Add Email id, to which we will send email
+                Msg.Body = EmailMessage.Text;
+                Msg.IsBodyHtml = true;
+                Msg.Priority = MailPriority.High;
+                SmtpClient smtp = new SmtpClient();
+                smtp.UseDefaultCredentials = false; // to get rid of error "SMTP server requires a secure connection"
+                smtp.Host = "smtp.gmail.com";
+                smtp.Port = 587;
+                smtp.Credentials = new System.Net.NetworkCredential("Akangtoshi@gmail.com", "password");// replace with valid value
+                smtp.EnableSsl = true;
+                smtp.Timeout = 20000;
+
+                smtp.Send(Msg);
+            }
+            catch(Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
         }
     }
 }
